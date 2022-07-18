cb895db1aa300118cd5d397a75097e256be4e457
gpdoud <gpdoud@gmail.com>
2022-07-18 08:18:47 -0400

    Initial commit

---
+++ DsiMiddleware.cs
@@ -0,0 +1,33 @@
+﻿using System;
+namespace DsiMiddleware {
+    public class DsiMiddlewareApiToken {
+
+        private readonly RequestDelegate _next;
+
+        public DsiMiddlewareApiToken(RequestDelegate next) {
+            _next = next;
+        }
+
+        private IConfigurationRoot GetAppsettings()
+            => new ConfigurationBuilder().AddJsonFile("appsettings.json").Build();
+
+
+        public async Task InvokeAsync(Microsoft.AspNetCore.Http.HttpContext http) {
+            var token = GetAppsettings().GetValue<string>("AppSettings:token");
+            var apikey = http.Request.Headers["apikey"];
+            if (token == apikey) {
+                await _next(http);
+            } else {
+                http.Response.StatusCode = 400;
+            }
+        }
+    }
+
+    public static class DsiMiddlewareExtensionMethods {
+
+        public static void UseDsiMiddleware(this WebApplication app) {
+            app.UseMiddleware<DsiMiddleware.DsiMiddlewareApiToken>();
+        }
+    }
+}
+
---
+++ Program.cs
@@ -0,0 +1,17 @@
+using DsiMiddleware;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+
+app.UseDsiMiddleware();
+
+app.MapGet("/test", (HttpRequest request) => {
+    return Results.Ok("Test successful!");
+});
+
+app.Run();
